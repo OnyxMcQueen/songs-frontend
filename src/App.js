bea54051fc00edab23448be280305a020b4c2a00
Onyx McQueen <onyxmcqueen88@gmail.com>
2023-08-03 05:21:07 -0400

    Added Page Loading State

--- src/App.js
+++ src/App.js
@@ -1,28 +1,33 @@
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import './App.css';
 
-import Home from './components/Home';
-import Nav from './components/Nav';
-import Songs from './components/Songs';
-import Song from './components/Song';
-import EditSong from './components/EditSong';
-import NewSong from './components/NewSong';
-import Error from './components/Error';
+import Spinner from './components/common/spinner/Spinner';
+
+ const Home = React.lazy(() => import ('./components/Home'));
+ const Nav = React.lazy(() => import ('./components/Nav'));
+ const Songs = React.lazy(() => import ('./components/Songs'));
+ const Song = React.lazy(() => import ('./components/Song'));
+ const EditSong = React.lazy(() => import ('./components/EditSong'));
+ const NewSong = React.lazy(() => import ('./components/NewSong'));
+ const Error = React.lazy(() => import ('./components/Error'));
 
 function App() {
   return (
-    <Router>
-      <Nav />
-      <Routes>
-        <Route path='/' element={<Home />}/>
-        <Route path='/songs' element={<Songs />}/>
-        <Route path='/songs/:id' element={<Song />}/>
-        <Route path='/songs/edit/:id' element={<EditSong />}/>
-        <Route path='/songs/new' element={<NewSong />}/>
-        <Route path='*' element={<Error/>}/>
-      </Routes>
-    </Router>
+    <React.Suspense fallback={<Spinner />}>
+      <Router>
+        <Nav />
+        <Routes>
+          <Route path='/' element={<Home />}/>
+          <Route path='/songs' element={<Songs />}/>
+          <Route path='/songs/:id' element={<Song />}/>
+          <Route path='/songs/edit/:id' element={<EditSong />}/>
+          <Route path='/songs/new' element={<NewSong />}/>
+          <Route path='*' element={<Error/>}/>
+        </Routes>
+      </Router>
+    </React.Suspense>
   );
 }
 
---
+++ src/components/common/spinner/Spinner.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import './spinner.css'
+
+function Spinner() {
+  return (
+    <div className='loader'>Loading...</div>
+  )
+}
+
+export default Spinner
